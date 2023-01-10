a09bbedb581873d7cc727fa03f4160d57770e3d9
psarvaiya <112694225+psarvaiya@users.noreply.github.com>
2023-01-10 13:45:41 +0530

    Update cf-change-set.sh

--- cf-change-set.sh
+++ cf-change-set.sh
@@ -7,44 +7,44 @@ function quit_execution(){
 
 function execute_change_set(){
         aws cloudformation execute-change-set  --change-set-name $(cat changeset.name) --stack-name sds-internal-change-sets
-        aws cloudformation wait stack-update-complete --stack-name sds-internal-change-sets
+        aws cloudformation wait stack-update-complete --stack-name $(cat stack.name)
 
 }
 
 function get_stack_events(){
-        aws cloudformation describe-stack-events --stack-name sds-internal-change-sets  --query StackEvents[*].[Timestamp,LogicalResourceId,ResourceStatus,ResourceStatusReason] --output table
+        aws cloudformation describe-stack-events --stack-name $(cat stack.name)  --query StackEvents[*].[Timestamp,LogicalResourceId,ResourceStatus,ResourceStatusReason] --output table
 
-        aws cloudformation describe-stack-events --stack-name sds-internal-change-sets  --query StackEvents[*].[Timestamp,LogicalResourceId,ResourceStatus,ResourceStatusReason] --output table > get_stack_events.txt 
+        aws cloudformation describe-stack-events --stack-name $(cat stack.name)  --query StackEvents[*].[Timestamp,LogicalResourceId,ResourceStatus,ResourceStatusReason] --output table > get_stack_events.txt 
 }
 
 
 function get_change_sets(){
         print_color 'green' "Fetching change sets"
-        aws cloudformation  describe-change-set --change-set-name $(cat changeset.name) --stack-name sds-internal-change-sets --query ExecutionStatus --query Changes[*].ResourceChange.[Action,LogicalResourceId,PhysicalResourceId] --output table
+        aws cloudformation  describe-change-set --change-set-name $(cat changeset.name) --stack-name $(cat stack.name) --query ExecutionStatus --query Changes[*].ResourceChange.[Action,LogicalResourceId,PhysicalResourceId] --output table
 
-        aws cloudformation  describe-change-set --change-set-name $(cat changeset.name) --stack-name sds-internal-change-sets --query ExecutionStatus --query Changes[*].ResourceChange.[Action,LogicalResourceId,PhysicalResourceId] --output table > get_change_sets.txt
+        aws cloudformation  describe-change-set --change-set-name $(cat changeset.name) --stack-name $(cat stack.name) --query ExecutionStatus --query Changes[*].ResourceChange.[Action,LogicalResourceId,PhysicalResourceId] --output table > get_change_sets.txt
 
 
 }
 
 function list_stack_resources(){
-        aws cloudformation list-stack-resources --stack-name sds-internal-change-sets --query StackResourceSummaries[*].[LogicalResourceId,PhysicalResourceId,ResourceType] --output table
+        aws cloudformation list-stack-resources --stack-name $(cat stack.name) --query StackResourceSummaries[*].[LogicalResourceId,PhysicalResourceId,ResourceType] --output table
 
-        aws cloudformation list-stack-resources --stack-name sds-internal-change-sets --query StackResourceSummaries[*].[LogicalResourceId,PhysicalResourceId,ResourceType] --output table > list_stack_resources.txt
+        aws cloudformation list-stack-resources --stack-name $(cat stack.name) --query StackResourceSummaries[*].[LogicalResourceId,PhysicalResourceId,ResourceType] --output table > list_stack_resources.txt
 
 }
 
 
 function display_output(){
-        aws cloudformation describe-stacks --stack-name sds-internal-change-sets --query Stacks[*].Outputs[*] --output table
+        aws cloudformation describe-stacks --stack-name $(cat stack.name) --query Stacks[*].Outputs[*] --output table
 
-        aws cloudformation describe-stacks --stack-name sds-internal-change-sets --query Stacks[*].Outputs[*] --output table > output.txt
+        aws cloudformation describe-stacks --stack-name $(cat stack.name) --query Stacks[*].Outputs[*] --output table > output.txt
 
 }
 
 
 function check_changeset_available(){
-        check_available=$(aws cloudformation  describe-change-set --change-set-name $(cat changeset.name) --stack-name sds-internal-change-sets --query ExecutionStatus --output text)
+        check_available=$(aws cloudformation  describe-change-set --change-set-name $(cat changeset.name) --stack-name $(cat stack.name) --query ExecutionStatus --output text)
 
 }
 
@@ -56,17 +56,17 @@ function loop_parameters(){
 
 
 function create_change_set(){
-        aws cloudformation create-change-set --stack-name sds-internal-change-sets --change-set-name $(cat changeset.name) --parameters file://parameters-dev.json --template-body file://cf-template.yaml --capabilities CAPABILITY_NAMED_IAM
+        aws cloudformation create-change-set --stack-name $(cat stack.name) --change-set-name $(cat changeset.name) --parameters file://parameters-dev.json --template-body file://cf-template.yaml --capabilities CAPABILITY_NAMED_IAM
 
         if [[ $? -eq 255 ]]; then
                 print_color 'red' 'PARAMETERS passed are incorrect, exiting the script'
                 quit_execution
         fi
 
-        aws cloudformation create-change-set --stack-name sds-internal-change-sets --change-set-name $(cat changeset.name) --parameters file://parameters-dev.json --template-body file://cf-template.yaml --capabilities CAPABILITY_NAMED_IAM > create_change_set.txt
+        aws cloudformation create-change-set --stack-name $(cat stack.name) --change-set-name $(cat changeset.name) --parameters file://parameters-dev.json --template-body file://cf-template.yaml --capabilities CAPABILITY_NAMED_IAM > create_change_set.txt
 
 cat > create_change_set_command.txt <<-EOF
-aws cloudformation create-change-set --stack-name sds-internal-change-sets --change-set-name $(cat changeset.name) --parameters file://parameters-dev.json --template-body file://cf-template.yaml --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-change-set --stack-name $(cat stack.name) --change-set-name $(cat changeset.name) --parameters file://parameters-dev.json --template-body file://cf-template.yaml --capabilities CAPABILITY_NAMED_IAM
 EOF
 
 }
